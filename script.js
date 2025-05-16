e38fa5db9adcd720d58ab3d376e2a5e61b924534
chitti239 <sharinis239@gmail.com>
2025-05-16 10:49:00 +0530

    Assignment completed

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const encrypt = (payload, secret) => {
   // your code here and return token
+  const encrypt = (payload, secret) => {
+  const token = jwt.sign(payload, secret, { expiresIn: '1h' });
+  return token;
+};
+
 };
 
 module.exports = encrypt;
